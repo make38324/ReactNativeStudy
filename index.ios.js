3da89c6c2ca16011ed9f97d82a11017898a12691
czmmac <make38324@163.com>
2016-09-21 15:23:46 +0800

    touch相关

--- index.ios.js
+++ index.ios.js
@@ -20,31 +20,37 @@ var {width,height,scale}=Dimensions.get('window');
 /**
  * Es5写法
  */
-//var HelloRN=React.createClass({
-//    render() {
-//        return (
-//            <View style={styles.container}>
-//                <View onPress={this.processPress("点击")}>
-//                    <TouchableOpacity activeOpacity={0.2}>
-//                        <Text style={{marginTop: 10,backgroundColor: 'blue',width:width}}>点击变色</Text>
-//                    </TouchableOpacity>
-//                    <Text>{this.state.title}</Text>
-//                </View>
-//            </View>
-//        );
-//    },
-//    getInitialState(){
-//        return {
-//            title:"不透明触摸"
-//        }
-//    },
-//    processPress(event) {
-//        //AlertIOS.alert("被点击");
-//        this.setState({
-//            title:event
-//        })
-//    }
-//})
+var HelloRN=React.createClass({
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <TouchableOpacity
+                        activeOpacity={0.2}
+                        onPress={()=>this.processPress("点击")}
+                        onLongPress={()=>this.processPress("长按")}
+                        onPressIn={()=>this.processPress("按下")}
+                        onPressOut={()=>this.processPress("抬起")}
+                        >
+                        <Text style={{marginTop: 10,backgroundColor: 'blue',width:width}}>点击变色</Text>
+                    </TouchableOpacity>
+                    <Text>{this.state.title}</Text>
+                </View>
+            </View>
+        );
+    },
+    getInitialState(){
+        return {
+            title:"不透明触摸"
+        }
+    },
+    processPress(event) {
+        //AlertIOS.alert("被点击");
+        this.setState({
+            title:event
+        })
+    }
+})
 /**
  * Es6写法
  */
@@ -65,24 +71,7 @@ var {width,height,scale}=Dimensions.get('window');
 //        AlertIOS.alert("被点击");
 //    }
 //}
-class HelloRN extends Component {
-    constructor() {
-        super();
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows(['row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2']),
-        };
-    }
 
-    render() {
-        return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={(rowData) => <Text>{rowData}</Text>}
-                />
-        );
-    }
-}
 const styles = StyleSheet.create({
     container: {
         marginTop: 20,
