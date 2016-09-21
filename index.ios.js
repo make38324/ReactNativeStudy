ed5d0a00c4ac3f0d8f4005f55e92d76627cc096a
czmmac <make38324@163.com>
2016-09-21 15:01:25 +0800

    ds

--- index.ios.js
+++ index.ios.js
@@ -10,39 +10,91 @@ import {
     StyleSheet,
     TextInput,
     Text,
-    View
+    TouchableOpacity,
+    AlertIOS,
+    View,
+    ListView
 } from 'react-native';
-var Dimensions=require('Dimensions');
+var Dimensions = require('Dimensions');
 var {width,height,scale}=Dimensions.get('window');
-
+/**
+ * Es5写法
+ */
+//var HelloRN=React.createClass({
+//    render() {
+//        return (
+//            <View style={styles.container}>
+//                <View onPress={this.processPress("点击")}>
+//                    <TouchableOpacity activeOpacity={0.2}>
+//                        <Text style={{marginTop: 10,backgroundColor: 'blue',width:width}}>点击变色</Text>
+//                    </TouchableOpacity>
+//                    <Text>{this.state.title}</Text>
+//                </View>
+//            </View>
+//        );
+//    },
+//    getInitialState(){
+//        return {
+//            title:"不透明触摸"
+//        }
+//    },
+//    processPress(event) {
+//        //AlertIOS.alert("被点击");
+//        this.setState({
+//            title:event
+//        })
+//    }
+//})
+/**
+ * Es6写法
+ */
+//class HelloRN extends Component {
+//    render() {
+//        return (
+//            <View style={styles.container}>
+//                <View onPress={this.processPress()}>
+//                    <TouchableOpacity activeOpacity={0.2}>
+//                        <Text style={{marginTop: 10,backgroundColor: 'blue',width:width}}>点击变色</Text>
+//                    </TouchableOpacity>
+//                </View>
+//            </View>
+//        );
+//    }
+//
+//    processPress() {
+//        AlertIOS.alert("被点击");
+//    }
+//}
 class HelloRN extends Component {
-  render() {
-    return (
-        <View style={styles.container}>
-          <TextInput
-              style={styles.contain_text}
-              keyboardType={'number-pad'}
-              autoFocus={true}
-              clearButtonMode={"always"}
-              placeholder={'我是占位文字'}
-              defaultValue="ok"
-              />
-        </View>
-    );
-  }
-}
+    constructor() {
+        super();
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(['row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2','row 1', 'row 2']),
+        };
+    }
 
+    render() {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={(rowData) => <Text>{rowData}</Text>}
+                />
+        );
+    }
+}
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  contain_text:{
-    height:60,
-    borderColor:"cyan",
-    borderWidth:1,
-    width:width,
-      marginTop:20
-  },
+    container: {
+        marginTop: 20,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    contain_text: {
+        height: 60,
+        borderColor: "cyan",
+        borderWidth: 1,
+        width: width,
+        marginTop: 20
+    },
 });
 AppRegistry.registerComponent('HelloRN', () => HelloRN)
